736fb70fc41c8ecaed698696387ee548b8a028a6
mesfint <mesfin_meng@yahoo.com>
2023-11-13 09:28:40 +0200

    database updated

--- src/webhook/index.ts
+++ src/webhook/index.ts
@@ -8,12 +8,16 @@ const stripeClient = new stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export const webhookHandler = async (req: Request, res: Response) => {
   try {
+    // Extract Stripe signature from request headers
     const sig = req.headers["stripe-signature"] as string;
+
+    // Construct the Stripe webhook event
     let event = stripeClient.webhooks.constructEvent(
       req.body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
+    // Handle different webhook event types
     if (event.type === "charge.succeeded") {
       const charge = event.data.object as stripe.Charge;
       const order = await Order.findOne({
@@ -25,6 +29,8 @@ export const webhookHandler = async (req: Request, res: Response) => {
         await order.save();
       }
     } else if (event.type === "charge.failed") {
+      // Handle failed charge event
+
       const charge = event.data.object as stripe.Charge;
       const order = await Order.findOne({
         paymentIntentId: charge.payment_intent,
@@ -35,8 +41,11 @@ export const webhookHandler = async (req: Request, res: Response) => {
         await order.save();
       }
     }
+    // Send a response indicating successful processing of the webhook event
+
     res.send({ received: true });
   } catch (error) {
+    // Handle and log any errors that occur during webhook processing
     console.log("error in webhookHandler", error);
     throw error;
   }
